e47dc554bcb02b0b6901718240d919d6dc82720c
naberyou <44559993+naberyou@users.noreply.github.com>
2018-10-29 20:41:51 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ function loadText(){
 //-1行づつ配列に格納 & ステータスチェック（読み終わり検知） ----------//
 //---------------------------------------------------------------//
 function checkStatus(){
+	alert("hit);
   if (xmlHttp.readyState == 4 && xmlHttp.status == 200){           //github up 用
 //  if (xmlHttp.readyState == 4 && xmlHttp.status == 0){               //ローカル環境用
     lines = xmlHttp.responseText.split( '\n' );
